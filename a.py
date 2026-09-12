363efa159075a5d7367ced2c20e79f6f7a0b9c36
A4molaxd <a4molaxd@gmail.com>
2026-09-12 11:37:05 +0200

    Deleted teset file

--- a.py
+++
@@ -1,6 +0,0 @@
-a = 13
-print(bin(3)[2:])
-print(bin(3)[2:].zfill(3))
-print(bin(3)[2:].zfill(3)[::-1])
-print(list(bin(3)[2:].zfill(3)[::-1]))
-print(int("".join(list(bin(5)[2:].zfill(3)[::-1])), 2))
